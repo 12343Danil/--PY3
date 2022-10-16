f5823e7bcddb98185475771ddb0f558cf346db81
Данил Гришин <115587027+12343Danil@users.noreply.github.com>
2022-10-16 20:08:56 +0300

    Add files via upload

---
+++ лабораторная1/lab3_task3.py
@@ -0,0 +1,11 @@
+condition = ...  # TODO поочередно подставьте значения True и False
+if condition:
+    print("Условие истинно")
+else:
+    print("Условие ложно")
+src = not False and True or False and not True
+src = True and True or False and False
+src = True or False # 1+0=1
+result = True  # TODO подставить результат выражения
+
+print(src == result)
