b85d49573635741cad38fd4220d6e6f4f0dbabf2
Данил Гришин <115587027+12343Danil@users.noreply.github.com>
2022-10-16 20:17:17 +0300

    Add files via upload

---
+++ лабораторная3/lab3_task1.py
@@ -0,0 +1,10 @@
+src = not False and True or False and not True
+
+# TODO расписать упрощение выражения
+
+result = ...  # TODO подставить результат выражения
+src = True and True or False and False
+src = True or False # 1+0=1
+result = True  # TODO подставить результат выражения
+
+print(src == result)
